91224c2da28f642b785769afbd3a54c9c7c65c01
XezXey <puntawat-1@hotmail.com>
2022-05-23 22:02:17 +0700

    up

--- sample_scripts/py/relighting/relighting_experiment_itc/sampling_lighting_itc.py
+++ sample_scripts/py/relighting/relighting_experiment_itc/sampling_lighting_itc.py
@@ -47,6 +47,8 @@ from sample_utils import (
 if __name__ == '__main__':
     # Load ckpt
     ckpt_loader = ckpt_utils.CkptLoader(log_dir=args.log_dir, cfg_name=args.cfg_name)
+    print(ckpt_loader.device)
+    exit()
     cfg = ckpt_loader.cfg
     seed_all(47)
     
