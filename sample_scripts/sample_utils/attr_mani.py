83af637663da6373bf0dcdad5ee0fc8e5ee4c177
XezXey <puntawat-1@hotmail.com>
2022-07-03 18:26:43 +0700

    adding gaussian weighted average

--- sample_scripts/sample_utils/attr_mani.py
+++ sample_scripts/sample_utils/attr_mani.py
@@ -12,12 +12,16 @@ class LinearClassifier(th.nn.Module):
         output = self.cls(x)
         return output
     
-    def cal_loss(self, gt, pred):
-        loss_fn = th.nn.BCEWithLogitsLoss()
+    def cal_loss(self, gt, pred, weighted_loss):
+        if weighted_loss is not None:
+            loss_fn = th.nn.BCEWithLogitsLoss(weight=weighted_loss)
+        else:
+            loss_fn = th.nn.BCEWithLogitsLoss()
+
         loss = loss_fn(pred, gt)
         return loss
 
-    def train(self, gt, input, n_iters, progress=True):
+    def train(self, gt, input, n_iters, weighted_loss, progress=True):
         print(f"[#] Training Linear Classifier with iterations={n_iters}, samples_size={gt.shape[0]}")
         if progress:
             import tqdm
@@ -27,7 +31,7 @@ class LinearClassifier(th.nn.Module):
         for i in t:
             self.opt.zero_grad()
             pred = self.forward(input)
-            loss = self.cal_loss(gt=gt, pred=pred)
+            loss = self.cal_loss(gt=gt, pred=pred, weighted_loss=weighted_loss)
             loss.backward()
             self.opt.step()
             if i % 500 == 0 and progress:
@@ -47,7 +51,25 @@ def distance(a, b, dist_type='l2'):
     elif dist_type == 'l2':
         return np.sum((a-b)**2)
 
+def get_weighted_average(dist_arr, sigma):
+    weighted_term = 1/np.sqrt(2 * np.pi * (sigma**2))* (np.exp(-np.power(dist_arr, 2.) / (2 * np.power(sigma, 2.))))
+    # import matplotlib.pyplot as plt
+    # plt.plot(weighted_term)
+    # plt.show()
+    # plt.plot(dist_arr)
+    # plt.show()
+    return weighted_term
+
 def retrieve_topk_params(params_set, ref_params, cfg, img_dataset_path, dist_type='l2', k=30):
+    '''
+    Return images and params of top-k nearest parameters
+    :param params_set: parameters in dict-like e.g. {'0.jpg':{'light' : ..., 'pose' : ..., ...}}
+    :param ref_params: reference parameters in numpy array
+    :param cfg: config file (only use in loading image function)
+    :param img_dataset_path: datapath to image
+    :param dist_type: distance type used in calculate the top-k nearest
+    :param k: Number of nearest-neighbour
+    '''
 
     light_dist = []
     img_name_list = []
@@ -55,12 +77,14 @@ def retrieve_topk_params(params_set, ref_params, cfg, img_dataset_path, dist_typ
         light_dist.append(distance(params_set[img_name]['light'], ref_params, dist_type=dist_type))
         img_name_list.append(img_name)
 
+
     min_idx = np.argsort(light_dist)[:k]
+    weighted_term = get_weighted_average(np.array(light_dist)[min_idx], sigma=1)
 
     img_path = file_utils._list_image_files_recursively(img_dataset_path)
     img_path = [img_path[i] for i in min_idx]
     img_name = [path.split('/')[-1] for path in img_path]
 
     images = mani_utils.load_image(all_path=img_path, cfg=cfg, vis=True)['image']
-    params_filtered = {k:params_set[k] for k in img_name}
-    return images, params_filtered
+    params_filtered = {i:params_set[i] for i in img_name}
+    return images, params_filtered, th.tensor(weighted_term)
