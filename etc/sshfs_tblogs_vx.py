34f98a22482aa1cadd3fe6f2f3606a0f519fc265
XezXey <puntawat-1@hotmail.com>
2022-09-05 16:03:34 +0700

    update ssh script

--- etc/sshfs_tblogs_vx.py
+++ etc/sshfs_tblogs_vx.py
@@ -10,14 +10,14 @@ assert os.path.isabs(args.folder)
 print("[#] Auto sshfs to ...", [f"v{id}" for id in args.vid])
 
 for id in args.vid:
-    print(f"========== V{id} (mint@10.204.100.1{int(id+10)}) ==========")
+    print(f"========== V{id} (mint@10.204.100.1{int(id+10)} / (mint@10.0.0.{int(id+10)}) ==========")
     print(f"[#] umount v{id}", end='')
     umount = f"sudo umount {args.folder}/v{id}"
     os.system(umount)
     print("... Done!")
-    
+
     print(f"[#] mounting v{id}", end='')
-    cmd = f"sshfs mint@10.0.0.1{int(id+10)}:/home/mint/guided-diffusion/tb_logs/ {args.folder}/v{id}"
+    cmd = f"sshfs mint@10.0.0.{int(id+10)}:/home/mint/guided-diffusion/tb_logs/ {args.folder}/v{id}"
     os.system(cmd)
     print("... Done!")
 
