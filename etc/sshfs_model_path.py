4519dcd0197f16945f24127ffb24360c0416f2d9
XezXey <puntawat-1@hotmail.com>
2022-09-19 21:51:55 +0700

    update mounting script

--- etc/sshfs_model_path.py
+++ etc/sshfs_model_path.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--vid', type=int, nargs='+')
@@ -13,8 +14,10 @@ print("[#] Auto sshfs to ...", [f"v{id}" for id in args.vid])
 for id in args.vid:
     print(f"========== V{id} (mint@10.204.100.1{int(id+10)} / mint@10.0.0.{int(id+10)}) ==========")
     print(f"[#] umount v{id}", end='')
-    umount = f"sudo umount {args.folder}/v{id}"
-    os.system(umount)
+    
+    mount_path = f'{args.folder}/v{id}'
+    umount = f"umount {mount_path}"
+    processes = subprocess.run(umount.split(' '))
     print("... Done!")
 
     print(f"[#] mounting v{id}", end='')
@@ -22,7 +25,10 @@ for id in args.vid:
       ip = f"10.0.0.{int(id+10)}"
     else:
       ip = f"10.204.100.{int(id+10)}"
-    cmd = f"sshfs mint@{ip}:/data/mint/model_logs/ {args.folder}/v{id}"
-    os.system(cmd)
+      
+    if not os.path.exists(mount_path):
+        os.makedirs(mount_path)
+    cmd = f"sshfs mint@{ip}:/data/mint/model_logs/ {mount_path}"
+    processes = subprocess.run(cmd.split(' '))
     print("... Done!")
 
