4c51090456b92edd8a58ebdcfd082c113ed5e085
XezXey <puntawat-1@hotmail.com>
2022-09-19 22:24:26 +0700

    update logging script

---
+++ etc/sshfs_logs.py
@@ -0,0 +1,48 @@
+import argparse
+import os
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--vid', type=int, nargs='+')
+parser.add_argument('--folder', type=str)
+parser.add_argument('--local', action='store_true', default=False)
+parser.add_argument('--tb_dir', default=None)
+parser.add_argument('--model_dir', default=None)
+args = parser.parse_args()
+
+assert os.path.isabs(args.folder)
+print("[#] Auto sshfs to ...", [f"v{id}" for id in args.vid])
+
+def mounting(from_path, to_path):
+    for id in args.vid:
+        print(f"========== V{id} (mint@10.204.100.1{int(id+10)} / mint@10.0.0.{int(id+10)}) ==========")
+        mount_path = f'{to_path}/v{id}'
+        
+        if not os.path.exists(mount_path):
+            os.makedirs(mount_path, exist_ok=True)
+            
+        if len(os.listdir(mount_path)) != 0:
+            print(f"[#] Umounting : {mount_path}")
+            umount = f"umount {mount_path}"
+            processes = subprocess.run(umount.split(' '))
+            print("... Done!")
+
+        if args.local:
+            ip = f"10.0.0.{int(id+10)}"
+        else:
+            ip = f"10.204.100.1{int(id+10)}"
+        
+        cmd = f"sshfs mint@{ip}:{from_path} {mount_path}"
+        print(f"Mounting : {cmd}")
+        processes = subprocess.run(cmd.split(' '))
+        print("... Done!")
+    print("#"*100)
+
+if __name__ == '__main__':
+    if args.tb_dir is not None:
+        print("[#] Mouting the tensorboard log from \"/guided-diffusion/tb_logs\"")
+        mounting(from_path='/home/mint/guided-diffusion/tb_logs/', to_path=args.tb_dir)
+    if args.model_dir is not None:
+        print("[#] Mouting the model log from \"/model_logs\"")
+        mounting(from_path='/data/mint/model_logs/', to_path=args.model_dir)
+        
--- etc/sshfs_tblogs_vx.py
+++ etc/sshfs_tblogs_vx.py
@@ -1,28 +1,38 @@
 import argparse
 import os
+import subprocess
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--vid', type=int, nargs='+', required=True)
-parser.add_argument('--folder', type=str, required=True)
+parser.add_argument('--vid', type=int, nargs='+')
+parser.add_argument('--folder', type=str)
+parser.add_argument('--local', action='store_true', default=False)
 args = parser.parse_args()
 
 assert os.path.isabs(args.folder)
 print("[#] Auto sshfs to ...", [f"v{id}" for id in args.vid])
 
 for id in args.vid:
-    print(f"========== V{id} (mint@10.204.100.1{int(id+10)} / (mint@10.0.0.{int(id+10)}) ==========")
-    print(f"[#] umount v{id}", end='')
-    
+    print(f"========== V{id} (mint@10.204.100.1{int(id+10)} / mint@10.0.0.{int(id+10)}) ==========")
     mount_path = f'{args.folder}/v{id}'
-    umount = f"sudo umount {mount_path}"
-    os.system(umount)
+    
+    if not os.path.exists(mount_path):
+        os.makedirs(mount_path, exist_ok=True)
+        
+    if len(os.listdir(mount_path)) != 0:
+        print(f"[#] Umounting : {mount_path}")
+        umount = f"umount {mount_path}"
+        processes = subprocess.run(umount.split(' '))
+        print("... Done!")
+
+    if args.local:
+      ip = f"10.0.0.{int(id+10)}"
+    else:
+      ip = f"10.204.100.1{int(id+10)}"
+      
+    cmd = f"sshfs mint@{ip}:{} {mount_path}"
+    print(f"Mounting : {cmd}")
+    processes = subprocess.run(cmd.split(' '))
     print("... Done!")
 
-    if not os.path.exists(mount_path):
-        os.makedirs(mount_path)
-        
-    print(f"[#] mounting v{id}", end='')
-    cmd = f"sshfs mint@10.0.0.{int(id+10)}:/home/mint/guided-diffusion/tb_logs/ {mount_path}"
-    os.system(cmd)
-    print("... Done!")
+
 
