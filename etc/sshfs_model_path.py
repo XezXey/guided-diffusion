e312e7eba5c299b3f9490421bd1b7715458da0f4
XezXey <puntawat-1@hotmail.com>
2022-09-19 22:03:40 +0700

    g

--- etc/sshfs_model_path.py
+++ etc/sshfs_model_path.py
@@ -24,11 +24,10 @@ for id in args.vid:
         processes = subprocess.run(umount.split(' '))
         print("... Done!")
 
-    print(f"[#] mounting v{id}", end='')
     if args.local:
       ip = f"10.0.0.{int(id+10)}"
     else:
-      ip = f"10.204.100.{int(id+10)}"
+      ip = f"10.204.100.1{int(id+10)}"
       
     cmd = f"sshfs mint@{ip}:/data/mint/model_logs/ {mount_path}"
     print(f"Mounting : {cmd}")
