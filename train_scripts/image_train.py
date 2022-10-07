ece7dfea8b405c6cf93b3cc830a17193f29daa8d
XezXey <puntawat-1@hotmail.com>
2022-10-08 06:28:18 +0700

    up

--- train_scripts/image_train.py
+++ train_scripts/image_train.py
@@ -19,7 +19,6 @@ def main():
     cfg = parse_args()
     seed_all(47)    # Seeding the model - Independent training
 
-    
     logger.configure(dir=cfg.train.log_dir)
     logger.log("[#] Creating model and diffusion...")
 
