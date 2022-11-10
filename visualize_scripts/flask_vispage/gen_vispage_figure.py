d5fd2d47709aab201ddba7ef1867cd216b52acf3
XezXey <puntawat-1@hotmail.com>
2022-11-11 04:39:09 +0700

    gggg

--- visualize_scripts/flask_vispage/gen_vispage_figure.py
+++ visualize_scripts/flask_vispage/gen_vispage_figure.py
@@ -6,6 +6,18 @@ import sys
 sys.path.insert(0, '/home/mint/guided-diffusion/sample_scripts/sample_utils/')
 import mani_utils, file_utils
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--exp_dir', required=True)
+parser.add_argument('--sample_dir', default="/home/mint/guided-diffusion/sample_scripts/py/relighting_sample_id/ddim_reverse_interpolate")
+parser.add_argument('--sample_pair_json', default="/home/mint/guided-diffusion/sample_scripts/py/relighting_sample_id/ddim_reverse_interpolate")
+parser.add_argument('--set_', default='valid')
+parser.add_argument('--res', default=128)
+parser.add_argument('--port', required=True)
+parser.add_argument('--host', default='0.0.0.0')
+args = parser.parse_args()
+
 def sort_by_frame(path_list):
     frame_anno = []
     for p in path_list:
@@ -535,8 +547,10 @@ def create_app():
         for s_id, src_dst in enumerate(subject_id):
             # out += create_hide(m, m_id)
             out += f"<tr>"
-            out += f"<th style=\"font-size:10px;white-space: nowrap;\"> {s_id+1}.({src_dst[0].split('.')[0]},{src_dst[1].split('.')[0]})<img src=/files/{data_path}/{src_dst[0].replace('jpg', 'png')} title=\"{src_dst[0]}\"><img src=/files/{data_path}/{src_dst[1].replace('jpg', 'png')} title=\"{src_dst[1]}\"> </th>"
-            # out += f"<td> {m_id+1} : {ckpt_dict[m]['alias']} <br> </td>"
+            if int(args.res) == 256:
+                out += f"<th style=\"font-size:10px;white-space: nowrap;\"> {s_id+1}.({src_dst[0].split('.')[0]},{src_dst[1].split('.')[0]})<img src=/files/{data_path}/{src_dst[0]} title=\"{src_dst[0]}\"><img src=/files/{data_path}/{src_dst[1]} title=\"{src_dst[1]}\"> </th>"
+            else:
+                out += f"<th style=\"font-size:10px;white-space: nowrap;\"> {s_id+1}.({src_dst[0].split('.')[0]},{src_dst[1].split('.')[0]})<img src=/files/{data_path}/{src_dst[0].replace('jpg', 'png')} title=\"{src_dst[0]}\"><img src=/files/{data_path}/{src_dst[1].replace('jpg', 'png')} title=\"{src_dst[1]}\"> </th>"
             for m_id, m in enumerate(model):
                 if ckpt == 'json':
                     vis_ckpt = step = ckpt_dict[m]['step']
@@ -545,8 +559,22 @@ def create_app():
                     
                 itp = ckpt_dict[m]['itp']
                 n_frames = ckpt_dict[m]['n_frames']
-                each_model = f"{args.sample_dir}/{args.exp_dir}/{m}/{step}/{args.set_}/{itp}/{sampling}_sampling/src={src_dst[0].replace('png', 'jpg')}/dst={src_dst[1].replace('png', 'jpg')}/"
+                if int(args.res) == 256:
+                    print("GGGGGGGGGGGGGGGGG")
+                    print(ckpt_dict[m]['step'])
+                    print(step)
+                    each_model = f"{args.sample_dir}/{args.exp_dir}/{m}/{step}/{args.set_}/{itp}/{sampling}_sampling/src={src_dst[0]}/dst={src_dst[1]}/"
+                else:
+                    each_model = f"{args.sample_dir}/{args.exp_dir}/{m}/{step}/{args.set_}/{itp}/{sampling}_sampling/src={src_dst[0].replace('png', 'jpg')}/dst={src_dst[1].replace('png', 'jpg')}/"
+                print("GGODDL")
+                print(each_model)
+                
                 frames = glob.glob(f"{each_model}/{itp_method}_{diff_step}/n_frames={n_frames}/{show}_f*.png")
+                print("HELP")
+                print(f"{each_model}/{itp_method}_{diff_step}/n_frames={n_frames}/")
+                print(frames)
+                print("PLS HELP")
+                
                 
                 out += "<td>"
                 if len(frames) > 0:
@@ -564,7 +592,7 @@ def create_app():
     def model_comparison_from_json_selector_lf():
         out = ""
         json_files = glob.glob('./json_comparison/experiment/*.json')
-        link_based = "itp_method=Slerp&diff_step=1000&sampling=reverse&ckpt=050000&show=res"
+        link_based = "itp_method=Lerp&diff_step=1000&sampling=reverse&ckpt=json&show=res"
         for jf in json_files:
             jf = jf.split('/')[-1]
             out += f"Comparison file : <a href=\"jf={jf}&{link_based}\">{jf}</a> <br>"
@@ -573,17 +601,6 @@ def create_app():
     return app
 
 if __name__ == "__main__":
-    import argparse
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--exp_dir', required=True)
-    parser.add_argument('--sample_dir', default="/home/mint/guided-diffusion/sample_scripts/py/relighting_sample_id/ddim_reverse_interpolate")
-    parser.add_argument('--sample_pair_json', default="/home/mint/guided-diffusion/sample_scripts/py/relighting_sample_id/ddim_reverse_interpolate")
-    parser.add_argument('--set_', default='valid')
-    parser.add_argument('--res', default=128)
-    parser.add_argument('--port', required=True)
-    parser.add_argument('--host', default='0.0.0.0')
-    args = parser.parse_args()
     
     
     data_path = f"/data/mint/DPM_Dataset/ffhq_256_with_anno/ffhq_{args.res}/{args.set_}/"
