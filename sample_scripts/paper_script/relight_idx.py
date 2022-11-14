1cab39fb0b2297747d2ea04609e33f2e68c1a65d
XezXey <puntawat-1@hotmail.com>
2022-11-15 02:47:29 +0700

    gg

--- sample_scripts/paper_script/relight_idx.py
+++ sample_scripts/paper_script/relight_idx.py
@@ -358,7 +358,9 @@ if __name__ == '__main__':
                 fps : video fps
             """
             #NOTE: save_video, w/ shape = TxHxWxC and value range = [0, 255]
+            print(out_relit.shape)
             vid_relit = th.cat((out_relit, th.flip(out_relit, dims=[0])))
+            print(vid_relit.shape)
             vid_relit = vid_relit.permute(0, 2, 3, 1)
             vid_relit = ((vid_relit + 1)*127.5).clamp_(0, 255).type(th.ByteTensor)
             torchvision.io.write_video(video_array=vid_relit, filename=f"{save_res_dir}/res.mp4", fps=args.fps)
