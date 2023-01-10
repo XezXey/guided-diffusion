d7d8a981de7d8270d78644060880043b22d78a18
XezXey <puntawat-1@hotmail.com>
2023-01-10 18:15:07 +0700

    update

--- sample_scripts/paper_script/relight_idx_supmat_without_MM.py
+++ sample_scripts/paper_script/relight_idx_supmat_without_MM.py
@@ -25,6 +25,12 @@ parser.add_argument('--src_dst', nargs='+', default=[], help='list of src and ds
 # Rendering
 parser.add_argument('--render_mode', type=str, default="shape")
 parser.add_argument('--rotate_normals', action='store_true', default=False)
+parser.add_argument('--scale_sh', type=float, default=1.0)
+parser.add_argument('--add_sh', type=float, default=None)
+parser.add_argument('--sh_grid_size', type=int, default=None)
+parser.add_argument('--sh_span', type=float, default=None)
+parser.add_argument('--diffuse_sh', type=float, default=None)
+parser.add_argument('--diffuse_perc', type=float, default=None)
 # Diffusion
 parser.add_argument('--diffusion_steps', type=int, default=1000)
 # Misc.
