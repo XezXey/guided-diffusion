6349a0c34eb8d77f900f497e9abe380068f23213
XezXey <puntawat-1@hotmail.com>
2023-03-06 02:57:01 +0700

    ggg

--- sample_scripts/paper_script/auto_run_shell/params_robustness/for_eval/run_mp.sh
+++ sample_scripts/paper_script/auto_run_shell/params_robustness/for_eval/run_mp.sh
@@ -11,7 +11,7 @@ for lvl in $nlvl
 do
     echo "[!] Running : "$pname - $lvl
     echo -e "\n\n"
-    python relight_idx_robust_params.py --dataset mp_valid --set valid --step 085000 --out_dir /data/mint/sampling/Robustness/MP/"$pname" --cfg_name Masked_Face_woclip+BgNoHead+shadow_256.yaml --log_dir Masked_Face_woclip+BgNoHead+shadow_256 --diffusion_steps "$diffusion" --seed 47 --sample_pair_json /home/mint/guided-diffusion/sample_scripts/paper_script/sample_json/experiment/ffhq_figure_original.json --sample_pair_mode pair --itp render_face --itp_step 2 --batch_size 10 --gpu_id "$gpu" --lerp --idx 0 30 --noisy_cond /home/mint/guided-diffusion/experiment_scripts/statistic_test/stat_ffhq_training_deca.json "$lvl" "$pname" --postfix "$pname"_"$lvl"
+    python relight_idx_robust_params.py --dataset mp_valid --set valid --step 085000 --out_dir /data/mint/sampling/Robustness/MP/"$pname" --cfg_name Masked_Face_woclip+BgNoHead+shadow_256.yaml --log_dir Masked_Face_woclip+BgNoHead+shadow_256 --diffusion_steps "$diffusion" --seed 47 --sample_pair_json /home/mint/guided-diffusion/sample_scripts/paper_script/sample_json/paper/multipie_valset.json --sample_pair_mode pair --itp render_face --itp_step 2 --batch_size 10 --gpu_id "$gpu" --lerp --idx 0 1000 --noisy_cond /home/mint/guided-diffusion/experiment_scripts/statistic_test/stat_ffhq_training_deca.json "$lvl" "$pname" --postfix "$pname"_"$lvl"
     echo "[!] Done : "$pname - $lvl
 done
 
