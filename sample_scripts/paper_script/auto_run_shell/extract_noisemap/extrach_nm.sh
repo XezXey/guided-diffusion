2e9f22da5983a9a980a2b4c6fe82eaa5ad938b70
XezXey <puntawat-1@hotmail.com>
2023-03-26 18:07:05 +0700

    update

--- sample_scripts/paper_script/auto_run_shell/extract_noisemap/extrach_nm.sh
+++ sample_scripts/paper_script/auto_run_shell/extract_noisemap/extrach_nm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 read -p "Input the sub_dataset to run (e.g. Anakin_2, Sull_1, Jon_1) : " dname
 read -p "Running on : " gpu
+read -p "Max-index : " max_idx
 cd ../../
 
 # Variables
@@ -10,7 +11,7 @@ for d in $dname
 do
     echo "[!] Running : "$d
     echo -e "\n\n"
-    python ./extract_noisemap/extract_noisemap.py --dataset Videos --sub_dataset "$d" --set valid --step 085000 --cfg_name Masked_Face_woclip+BgNoHead+shadow_256.yaml --log_dir Masked_Face_woclip+BgNoHead+shadow_256 --diffusion_steps 1000 --seed 47 --batch_size 5 --gpu_id "$gpu" --idx 0 1000
+    python ./extract_noisemap/extract_noisemap.py --dataset Videos --sub_dataset "$d" --set valid --step 085000 --cfg_name Masked_Face_woclip+BgNoHead+shadow_256.yaml --log_dir Masked_Face_woclip+BgNoHead+shadow_256 --diffusion_steps 1000 --seed 47 --batch_size 5 --gpu_id "$gpu" --idx 0 "$max_idx"
     echo "[!] Done : "$d
 done
 
