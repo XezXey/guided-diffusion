f5b2cc99487428ab2196bde22da331d44ac2d061
XezXey <puntawat-1@hotmail.com>
2023-04-05 21:09:43 +0700

    add flows

---
+++ experiment_scripts/smooth_flow/flow_smooth_kpts/compute_flow.py
@@ -0,0 +1,176 @@
+# Author: Deepak Pathak (c) 2016
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+# from __future__ import unicode_literals
+import numpy as np
+import time
+import argparse
+import sys
+import os
+import subprocess
+from tqdm import tqdm
+# Adding the pyflow path directories
+import pyflow
+import matplotlib.pyplot as plt
+import cv2
+from PIL import Image
+import multiprocessing
+import configparser
+import glob
+
+parser = argparse.ArgumentParser(description="Optical flow debugger by warpping the images")
+parser.add_argument('--image_dir', dest='image_dir', help='Input the image directories', required=True)
+parser.add_argument('--output_savepath', dest='output_savepath', help='Save path of warped images', required=True)
+parser.add_argument('--video_name', dest='video_name', help='current process video', required=True)
+parser.add_argument('--config_opticalflow_path', dest='config_opticalflow_path', help='path to a config_opticalflow.ini file')
+parser.add_argument('--chunk_size', dest='chunk_size', type=int, help='Chunking for multiprocess')
+args = parser.parse_args()
+
+# Flow Options:
+alpha = 0.012 # Smoothness of frame (Higher means small movement/difference will be captured => small optical flow offset)
+ratio = 0.75 # Decreasing rate of the image size per each pyramid
+minWidth = 20 # Size of receptive field for each image (Lower is better to capture a large movement)
+nOuterFPIterations = 7
+nInnerFPIterations = 1
+nSORIterations = 30
+colType = 0  # 0 or default:RGB, 1:GRAY (but pass gray image with shape (h,w,1))
+
+def flow_image(flow, im1):
+  hsv = np.zeros(im1.shape, dtype=np.uint8)
+  hsv[:, :, 0] = 255
+  hsv[:, :, 1] = 255
+  mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+  hsv[..., 0] = ang * 180 / np.pi / 2
+  hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+  rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+  return rgb
+  
+
+def compute_flow(i, im1, im2, normalize, output_optical_flow_path_fw, output_optical_flow_path_bw):
+  # Normalization flag for input images
+  if normalize:
+    im1 = im1.astype(float) / 255.
+    im2 = im2.astype(float) /255.
+  # Calculate a forward sequence optical flow 
+  u, v, im2W = pyflow.coarse2fine_flow(
+    im1, im2, alpha, ratio, minWidth, nOuterFPIterations, nInnerFPIterations, nSORIterations, colType)
+  flows_forward = np.concatenate((u[..., None], v[..., None]), axis=2)
+  # Create a Forward frame
+  # cv2.imwrite(f'{output_optical_flow_path_fw}outflow_warped_frame_{i}.png', np.concatenate((im1[:, :, ::-1]*255, im2[:, :, ::-1]*255, im2W[:, :, ::-1] * 255), axis=1))
+  # cv2.imwrite(f"{output_optical_flow_path_fw}frame_{i.split('_')[0]}.png", im1[:, :, ::-1] * 255)
+  # cv2.imwrite(f"{output_optical_flow_path_fw}frame_{i.split('_')[1]}.png", im2[:, :, ::-1] * 255)
+  # cv2.imwrite(f"{output_optical_flow_path_fw}outflow_warped_frame_{i}.png", im2W[:, :, ::-1] * 255)
+  # cv2.imwrite(f"{output_optical_flow_path_fw}outflow_frame_{i}.png", flow_image(flow=flows_forward, im1=im1))
+
+  # Calculate a backward sequence optical flow 
+  u, v, im2W = pyflow.coarse2fine_flow(
+    im2, im1, alpha, ratio, minWidth, nOuterFPIterations, nInnerFPIterations, nSORIterations, colType)
+  flows_backward = np.concatenate((u[..., None], v[..., None]), axis=2)
+  # Create a Backward frame
+  # cv2.imwrite(f"{output_optical_flow_path_bw}frame_{i.split('_')[0]}.png", im1[:, :, ::-1] * 255)
+  # cv2.imwrite(f"{output_optical_flow_path_bw}frame_{i.split('_')[1]}.png", im2[:, :, ::-1] * 255)
+  # cv2.imwrite(f"{output_optical_flow_path_bw}outflow_warped_frame_{i}.png", im2W[:, :, ::-1] * 255)
+  # cv2.imwrite(f"{output_optical_flow_path_bw}outflow_frame_{i}.png", flow_image(flow=flows_backward, im1=im1))
+  # cv2.imwrite(f'{output_optical_flow_path_bw}outflow_warped_frame_{i}.png', np.concatenate((im2[:, :, ::-1]*255, im1[:, :, ::-1]*255, im2W[:, :, ::-1] * 255), axis=1))
+  
+  
+  print(f"SHAPE of {i}: ", flows_forward.shape, flows_backward.shape)
+  return {i : {
+              'fw' : flows_forward, 
+              'bw' : flows_backward
+            }
+        }
+
+def optical_flow_estimation(frames_idx, images, config_opticalflow, output_path='./', normalize=True):
+  # Assign the value of parameters from dict.
+  global alpha, ratio, minWidth
+  alpha = config_opticalflow['alpha']
+  ratio = config_opticalflow['ratio']
+  minWidth = config_opticalflow['minWidth']
+  # Create an optical flow folder for saving a results
+  config_opticalflow_path = f"{alpha}alpha_{ratio}ratio_{minWidth}minWidth/"
+  output_optical_flow_path = f"{output_path}/{args.video_name}/{config_opticalflow_path}"
+  output_optical_flow_path_fw = f"{output_optical_flow_path}/Forward/"
+  output_optical_flow_path_bw = f"{output_optical_flow_path}/Backward/"
+  
+  # Create the directory
+  os.makedirs(output_optical_flow_path, exist_ok=True)
+  os.makedirs(output_optical_flow_path_fw, exist_ok=True)
+  os.makedirs(output_optical_flow_path_bw, exist_ok=True)
+
+  # Computing flows
+  pooling = multiprocessing.Pool(multiprocessing.cpu_count())
+  print("Optical Flow Estimation ...")
+  # Using multiprocessing pool to compute flows in parallel
+  frames_idx = [f'frame{frames_idx[i]}_frame{frames_idx[i+1]}' for i in range(len(frames_idx)-1)]
+  # print(frames_idx)
+  inputs = list(zip(frames_idx,
+              [images[i] for i in range(len(images)-1)],
+              [images[i+1] for i in range(len(images)-1)],
+              [normalize] * (len(images)-1),
+              [output_optical_flow_path_fw] * (len(images)-1),
+              [output_optical_flow_path_bw] * (len(images)-1),))
+  
+  chunk_size = args.chunk_size
+  input_chunks = []
+  for i in range(0, len(inputs), chunk_size):
+      input_chunks.append(inputs[i:i+chunk_size])
+      
+  flows_res = {}
+  count = 0
+  for ic in tqdm(input_chunks):
+      results = pooling.starmap(compute_flow, ic)
+      flows_res.update({k: v for d in results for k, v in d.items()})
+      np.save(file=f'{output_optical_flow_path}/{args.video_name}_sub_flows_{count}.npy', arr=results)
+      count+=1
+  
+  
+  flows_fw = []
+  flows_bw = []
+  for _, v in flows_res.items():
+    flows_fw.append(v['fw'])
+    flows_bw.append(v['bw'])
+    
+  flows_fw = np.stack(flows_fw, axis=0)
+  flows_bw = np.stack(flows_bw, axis=0)
+  # Summary and save to the output path
+  print("OPTICAL FLOW SUMMARY : ")
+  print("Forward Sequence FLOWS : ", flows_bw.shape)
+  print("Backward Sequence FLOWS : ", flows_fw.shape)
+  print("Saving optical_flow_estimation to : ", output_optical_flow_path)
+  np.save(file=f'{output_optical_flow_path}/{args.video_name}_flows.npy', arr=flows_res)
+
+def read_images(image_dir):
+  print("[#] Reading images...")
+  fn_list = glob.glob(image_dir + '/frame*.png')
+  fn_list = sorted(fn_list, key=lambda x:int(x.split('/')[-1][5:-4]))
+  frame_idx = []
+  imgs = []
+  for f in tqdm(fn_list):
+    imgs.append(np.array(Image.open(f)))
+    frame_idx.append(f.split('/')[-1][5:-4])
+  return imgs, frame_idx
+    
+    
+  
+
+if __name__ == '__main__':
+  # Running a flow estimation to given input/video
+  config = configparser.ConfigParser()
+  config.read(args.config_opticalflow_path)
+  output_path = args.output_savepath
+  image_dir = args.image_dir
+  alpha_list = config['CONFIG_OPTICALFLOW']['alpha'].split('\n')
+  ratio_list = config['CONFIG_OPTICALFLOW']['ratio'].split('\n')
+  minWidth_list = config['CONFIG_OPTICALFLOW']['minWidth'].split('\n')
+  images, frames_idx = read_images(image_dir)
+  for alpha in alpha_list:
+    for ratio in ratio_list:
+      for minWidth in minWidth_list:
+        config_name = '{}alpha_{}ratio_{}minWidth'.format(alpha, ratio, minWidth)
+        config_opticalflow = {'alpha':float(alpha),
+                              'ratio':float(ratio),
+                              'minWidth':float(minWidth)}
+        optical_flow_estimation(frames_idx, images, config_opticalflow, output_path)
+
---
+++ experiment_scripts/smooth_flow/flow_smooth_kpts/opt_flow_warp.py
@@ -0,0 +1,265 @@
+import numpy as np
+import argparse
+import glob, os
+import cv2
+import numpy as np
+from PIL import Image
+
+parser = argparse.ArgumentParser(
+  description="Optical flow debugger by warpping the images")
+parser.add_argument('--image_dir', dest='image_dir', help='Input the image directories', required=True)
+parser.add_argument('--flows', dest='flows', help='Input the path to flow .npy format file', required=True)
+parser.add_argument('--kpts', dest='kpts', help='Input the path to flow .npy format file', required=True)
+args = parser.parse_args()
+
+def computeChain(flow_list, init_kpts):
+  # Compute chain flows from given list of flows
+  # flow_list is in flow[i] to flow[j] that want to compose
+  # This function won't handle a backward_flow (Need to reverse before pass into flow_list)
+  # The output will on the lastest index of flow_chains
+  '''
+  flow_list : [flow_1, flow_2, ..., flow_n]
+  kpts : [kpt_1, kpt_2, ..., kpt_n]; eact kpt in shape (68, 2)
+  '''
+  kpts_flow = []  # Store the flow at each keypoints
+  for kx, ky in init_kpts:
+    kpts_flow.append(flow_list[0][ky][kx])
+  kpts_flow = np.array(kpts_flow)
+    
+  for i in range(len(flow_list)-1):
+    # flows_chains.append(compose_flow_at_kpt(v0=flow_list[i+1], v1=flows_chains[i], kpts=kpts_list[i]))
+    update_flow = compose_flow_at_kpt(v0=kpts_flow, v1=flow_list[i+1], kpts=init_kpts)
+    kpts_flow = update_flow
+    # print(update_flow)
+    # exit()
+  # print(kpts_flow.shape)
+  # print(kpts_flow)
+  # exit()
+  return kpts_flow
+
+def compose_flow_at_kpt(v0, v1, kpts):
+  ''' This function take 2 flows and compose it
+      v0 : ref <- a1
+      v1 : a1 <- a2
+      Full path : ref <- a1 <- a2
+      output : ref <- a2
+  '''
+  # For compose any 2 adjacent flow together.
+  composed_flow = np.zeros(shape=kpts.shape)
+  # print(composed_flow.shape)
+  for idx, kpt in enumerate(kpts):
+      # Find the landed pixels locations on v1 that jump from v0(offset) + keypoints position (pixel_ij)
+      i = kpt[0]
+      j = kpt[1]
+      # print(i, j, v0.shape)
+      nc = i + v0[idx][0]  # On x-axis
+      nr = j + v0[idx][1]  # On y-axis
+      # Flow on x-axis
+      composed_flow[idx][0] = v0[idx][0] + bilinear_interpolation(v1[..., 0], nr, nc)
+      # Flow on y-axis
+      composed_flow[idx][1] = v0[idx][1] + bilinear_interpolation(v1[..., 1], nr, nc)
+  # print("===>Composed flow : ", composed_flow.shape)
+  # print("======>V0 : ", v0.shape)
+  # print("======>V1 : ", v1.shape)
+  return composed_flow
+
+def warp_kpts(kpts, flows):
+  ''' Warp image function take 2 inputs agruments
+      1. img : src kpts
+      2. chain_flows : optical flow(list-like) from cummulative from src to dest images
+  '''
+  warped_kpts = np.zeros_like(kpts)
+  final_warp_flow = flows
+  # print("======> Final warp flow : ", final_warp_flow.shape)
+  # print("======> Kpts shape : ", kpts.shape)
+  # print("[*] Warping ...", end='')
+  for i in range(warped_kpts.shape[0]): # Each kpts
+        warped_kpts[i][0] = kpts[i, 0] + final_warp_flow[i][0]
+        warped_kpts[i][1] = kpts[i, 1] + final_warp_flow[i][1]
+  # print("Done!")
+  # print("======>Warped dest-kpts shape : ", warped_kpts.shape)
+  return warped_kpts
+
+def bilinear_interpolation(flow, x_pos, y_pos):
+    '''Interpolate (x, y) from values associated with four points.
+    Four points are in list of four triplets : (x_pos, y_pos, value)
+    - x_pos and y_pos is the pixels position on the images
+    - flow is the matrix at x_pos and y_pos to be a reference for interpolated point.
+    Ex : x_pos = 12, y_pos = 5.5
+         # flow is the matrix of source to be interpolated
+         # Need to form in 4 points that a rectangle shape
+         flow_location = [(10, 4, 100), === [(x1, y1, value_x1y1),
+                          (20, 4, 200), ===  (x2, y1, value_x2y1),
+                          (10, 6, 150), ===  (x1, y2, value_x1y2),
+                          (20, 6, 300)] ===  (x2, y2, value_x2y2)]
+        Reference : https://en.wikipedia.org/wiki/Bilinear_interpolation
+
+    '''
+    # Create a flow_location : clip the value from 0-flow.shape[0-or-1]-1
+    # x1, y1 : Not lower than 0
+    # x2, y2 : Not exceed img size
+    x1 = np.clip(int((np.floor(x_pos))), 0, flow.shape[0]-1)
+    x2 = np.clip(x1 + 1, 0, flow.shape[0]-1)
+    y1 = np.clip(int((np.floor(y_pos))), 0, flow.shape[1]-1)
+    y2 = np.clip(y1 + 1, 0, flow.shape[1]-1)
+    x_pos = np.clip(x_pos, 0, flow.shape[0]-1)
+    y_pos = np.clip(y_pos, 0, flow.shape[1]-1)
+
+    # Last pixels will be the problem that exceed the image size
+    if x1 == flow.shape[0]-1:
+        x1 = flow.shape[0]-2
+    if y1 == flow.shape[0]-1:
+        y1 = flow.shape[0]-2
+
+    # print("X : ", x_pos, x1, x2)
+    # print("Y : ", y_pos, y1, y2)
+    flow_area = [(x1, y1, flow[x1][y1]),
+                 (x2, y1, flow[x2][y1]),
+                 (x1, y2, flow[x1][y2]),
+                 (x2, y2, flow[x2][y2])]
+
+    # print("Flow interesting area : ", flow_area)threshold_slow
+    flow_area = sorted(flow_area)
+    (x1, y1, q11), (_x1, y2, q12), (x2, _y1, q21), (_x2, _y2, q22) = flow_area
+
+    if x1!=_x1 or x2!= _x2 or y1!=_y1 or y2!=_y2:
+        raise ValueError('Given grid do not form a rectangle.')
+    if not x1 <= x_pos <= x2 or not y1 <= y_pos <= y2:
+        raise ValueError('(x, y) that want to interpolated is not within the rectangle')
+
+    return (q11 * (x2-x_pos) * (y2-y_pos) +
+            q21 * (x_pos-x1) * (y2-y_pos) +
+            q12 * (x2-x_pos) * (y_pos-y1) +
+            q22 * (x_pos-x1) * (y_pos-y1)
+            / ((x2-x1) * (y2-y1)) + 0.0)
+  
+if __name__ == "__main__":
+  # Load flows
+  flows = np.load(args.flows, allow_pickle=True).item()
+  # flows = np.load(args.flows, allow_pickle=True).tolist()
+  # flows = {k: v for d in flows for k, v in d.items()}
+    
+    
+  # Load keypoints
+  load_kpts = np.load(args.kpts, allow_pickle=True).item()
+  kpts = {}
+  for k in load_kpts.keys():
+    kpts[k] = np.array(load_kpts[k]['face_landmark'])
+  
+  n_votes = 3
+  
+  smooth_kpts = {}
+  f_list = sorted(kpts.keys(), key=lambda x:int(x.split('/')[-1][5:-4]))
+  for i, f in enumerate(f_list):  # Process each frames
+    
+    # Get frames index that used as voters
+    fw_voters = np.clip(i+n_votes, 0, len(f_list))
+    bw_voters = np.clip(i-n_votes, 0, len(f_list))
+    fw_v_list = f_list[i+1:fw_voters+1]
+    bw_v_list = f_list[bw_voters:i]
+    
+    # Get the flows for specifics frame
+    if len(fw_v_list) != 0:
+      fw_kpts_list = fw_v_list
+      fw_v_list = [f] + fw_v_list
+    if len(bw_v_list) != 0:
+      bw_kpts_list = bw_v_list
+      bw_v_list = bw_v_list + [f]
+      
+    print("#" * 77)
+    print("[#] Frame : ", f)
+    print("[#] fw_voters : ", fw_v_list)
+    print("[#] bw_voters : ", bw_v_list)
+    
+    # print(flows.keys())
+    # flows_fw = []
+    # flows_bw = []
+    fw_v_list = [f"{fw_v_list[i].split('.')[0]}_{fw_v_list[i+1].split('.')[0]}" for i in range(len(fw_v_list)-1)]
+    bw_v_list = [f"{bw_v_list[i].split('.')[0]}_{bw_v_list[i+1].split('.')[0]}" for i in range(len(bw_v_list)-1)]
+    
+    print("[#] fw_flows_voters : ", fw_v_list)
+    print("[#] bw_flows_voters : ", bw_v_list)
+    
+    flows_fw = []
+    for k in fw_v_list:
+      flows_fw.append(flows[k]['fw'])
+      
+    flows_bw = []
+    for k in bw_v_list:
+      flows_bw.append(flows[k]['bw'])
+      
+    candi_kpts = [kpts[f]]
+    
+    if len(flows_fw) != 0:
+      #NOTE: Forward flows = warping from [t+1, t+2, ..., t+n_votes] ===> to "t" kpts
+      fw_out = []
+      for i, k in enumerate(fw_kpts_list):
+        chain = flows_fw[:i+1]
+        if len(chain) > 1:
+          chain = computeChain(chain, kpts[k])
+        else:
+          tmp = []
+          for kx, ky in kpts[k]:
+            tmp.append(chain[0][ky][kx])
+          chain = np.array(tmp)
+        out = warp_kpts(kpts[k], chain)
+        candi_kpts.append(out)
+      
+      
+    if len(flows_bw) != 0:
+      #NOTE: Backward flows = warping from [t-n_votes, ..., t-2, t-1] ===> to "t" kpts
+      fw_out = []
+      for i, k in enumerate(bw_kpts_list):
+        chain = flows_bw[:i+1]
+        if len(chain) > 1:
+          chain = computeChain(chain[::-1], kpts[k])
+        else:
+          tmp = []
+          for kx, ky in kpts[k]:
+            tmp.append(chain[0][ky][kx])
+          chain = np.array(tmp)
+        out = warp_kpts(kpts[k], chain)
+        candi_kpts.append(out)
+    
+    candi_kpts = np.stack(candi_kpts, axis=0)
+    print(f"{f} : shape={candi_kpts.shape}, mean={np.mean(candi_kpts, axis=0).shape}, sd={np.std(candi_kpts, axis=0).shape}")
+    smooth_kpts[f] = np.mean(candi_kpts, axis=0)
+    
+  print(smooth_kpts.keys())
+  
+  import align_lib
+  data_dir = '/data/mint/DPM_Dataset/Videos/joker_3/images/'
+  frames = sorted(kpts.keys(), key=lambda x:int(x[5:-4]))
+  aligned_vis = []
+  for f in frames:
+      aligned_img = align_lib.image_align(src_file=data_dir + f,
+                                face_landmarks=smooth_kpts[f], 
+                                output_size=256)
+      aligned_vis.append(aligned_img)
+
+  align_lib.save_video(aligned_vis, name='smth_aligned', fps=25)
+  
+ 
+  kpts_vis = []
+  for f in frames:
+      img = np.array(Image.open(data_dir + f))
+      lmk = align_lib.draw_face_landmarks(img, kpts[f])
+      kpts_vis.append(lmk)
+      
+  align_lib.save_video(kpts_vis, name='lmk')
+  
+  kpts_vis = []
+  for f in frames:
+      img = np.array(Image.open(data_dir + f))
+      lmk = align_lib.draw_face_landmarks(img, smooth_kpts[f])
+      kpts_vis.append(lmk)
+      
+  align_lib.save_video(kpts_vis, name='smth_lmk')
+  
+  kpts_vis = []
+  for f in frames:
+      img = np.array(Image.open(data_dir + f))
+      lmk = align_lib.draw_face_landmarks(img, kpts[f], smooth_kpts[f])
+      kpts_vis.append(lmk)
+      
+  align_lib.save_video(kpts_vis, name='cmp_lmk')
