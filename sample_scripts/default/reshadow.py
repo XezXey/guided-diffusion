568ddc857b75837a07f8959a49f226a00bbb13c1
XezXey <puntawat-1@hotmail.com>
2024-07-30 17:32:19 +0700

    gg

--- sample_scripts/default/reshadow.py
+++ sample_scripts/default/reshadow.py
@@ -31,6 +31,9 @@ parser.add_argument('--src_dst', nargs='+', default=[], help='list of src and ds
 # Rendering
 parser.add_argument('--render_mode', type=str, default="shape")
 parser.add_argument('--rotate_normals', action='store_true', default=False)
+parser.add_argument('--rotate_sh', action='store_true', default=False)
+parser.add_argument('--rotate_sh_axis', type=int, default=0)
+parser.add_argument('--spiral_sh', action='store_true', default=False)
 parser.add_argument('--scale_sh', type=float, default=1.0)
 parser.add_argument('--add_sh', type=float, default=None)
 parser.add_argument('--sh_grid_size', type=int, default=None)
