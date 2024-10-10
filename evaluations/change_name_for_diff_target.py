186c82a578bc70433ddf191f95fc48b1b9d3f3c1
XezXey <puntawat-1@hotmail.com>
2024-10-10 14:42:21 +0700

    gg

--- evaluations/change_name_for_diff_target.py
+++ evaluations/change_name_for_diff_target.py
@@ -16,32 +16,32 @@ else:
 with open(path, 'r') as f:
     data = json.load(f)['pair']
  
-for p in glob.glob(args.pred_path + '/*.png'):   
-    # if os.path.isdir(p):
-    os.makedirs(f"{p}/out_transf_eval/", exist_ok=True)
-    for k, v in data.items():
-        # Old filename: input=346_03_01_051_06.png#pred=090_01_01_051_03.png.png
-        
-        print(k, v)
-        pid = k
-        src = v['src']
-        ref = v['dst']
-        gt = v['gt']
-        
-        
-        fn = f"input={src}#pred={ref}.png"
-        fn_fp = f"{p}/out_eval/{fn}"
-        if not os.path.exists(fn_fp):
-            print(f"Missing: {fn}, ID: {pid}, path: {fn_fp}")
-            continue
-        else:
-            # new_fn = f'input={src}#ref={ref}#gt={gt}.png'
-            new_fn = f'input={src}#ref={ref}#pred={gt}.png'
-            new_fn_fp = f"{p}/out_transf_eval/{new_fn}"
-            os.system(f"cp {fn_fp} {new_fn_fp}")
+for p in glob.glob(args.pred_path + '/*'):   
+    if os.path.isdir(p):
+        os.makedirs(f"{p}/out_transf_eval/", exist_ok=True)
+        for k, v in data.items():
+            # Old filename: input=346_03_01_051_06.png#pred=090_01_01_051_03.png.png
+            
+            print(k, v)
+            pid = k
+            src = v['src']
+            ref = v['dst']
+            gt = v['gt']
+            
+            
+            fn = f"input={src}#pred={ref}.png"
+            fn_fp = f"{p}/out_eval/{fn}"
+            if not os.path.exists(fn_fp):
+                print(f"Missing: {fn}, ID: {pid}, path: {fn_fp}")
+                continue
+            else:
+                # new_fn = f'input={src}#ref={ref}#gt={gt}.png'
+                new_fn = f'input={src}#ref={ref}#pred={gt}.png'
+                new_fn_fp = f"{p}/out_transf_eval/{new_fn}"
+                os.system(f"cp {fn_fp} {new_fn_fp}")
+                
+                
+                
             
             
             
-        
-        
-        
