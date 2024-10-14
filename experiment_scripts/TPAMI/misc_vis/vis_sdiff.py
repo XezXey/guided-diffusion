985569c147b0c134385fb3ba7994d455ca5f4a5c
XezXey <puntawat-1@hotmail.com>
2024-10-14 14:24:43 +0700

    add extra shadows to samples

--- experiment_scripts/TPAMI/misc_vis/vis_sdiff.py
+++ experiment_scripts/TPAMI/misc_vis/vis_sdiff.py
@@ -25,6 +25,20 @@ if run:
         c_sorted = c.sort_values(by=['IOU'], ascending=False)
     run = False
 
+    max_c = c['c_val'].max()
+    min_c = c['c_val'].min()
+    print(f"Max: {max_c} Min: {min_c}")
+    # print("[#] Loading the generated shadow dataset")
+    # # Read the sample file
+    # with open('/home/mint/Dev/DiFaReli/difareli-faster/dataset_generation/sampler/sampling_generated_dataset/generated_dataset_1_seed=47.json') as f:
+    #     gen_shadow = json.load(f)['pair']
+    # gen_shadow_dict = {}
+    # for k, v in gen_shadow.items():
+    #     if v['src'] not in list(gen_shadow_dict.keys()):
+    #         gen_shadow_dict[v['src']] = [v['dst']]
+    #     else:
+    #         gen_shadow_dict[v['src']].append(v['dst'])
+
 def create_app():
     app = Flask(__name__)
     
@@ -53,6 +67,7 @@ def create_app():
         # train_sub/train_0_to_5000/shadow/reverse_sampling
         strengthen_path = '/data/mint/DPM_Dataset/Soften_Strengthen_Shadows/TPAMI/FFHQ_shadow_face/log=difareli_canny=153to204bg_256_vll_cfg=difareli_canny=153to204bg_256_vll.yaml_tomax_steps=50/ema_085000/'   
         diffuse_path = '/data/mint/DPM_Dataset/Soften_Strengthen_Shadows/TPAMI/FFHQ_diffuse_face/log=Masked_Face_woclip+BgNoHead+shadow_256_cfg=Masked_Face_woclip+BgNoHead+shadow_256.yaml_tomin_steps=50/ema_085000/'
+
         
         for img_path in all_path[int(s):int(e)]:
             print(img_path)
@@ -75,9 +90,28 @@ def create_app():
                 
                 tp = f"{diffuse_path}/train_sub/train_{sub[0]}_to_{sub[1]}/shadow/reverse_sampling/src={img_name}.jpg/dst={sub[0]}.jpg/Lerp_1000/n_frames=3/res_frame2.png"
                 out += f"<img src=/files/{tp} width=\"256\" height=\"256\">"
+
+            out += "<br>"
+            
+            # Generated dataset
+            for dst_p in glob.glob(f"/data/mint/dataset_generation/cast_shadows/log=DiFaReli_FsBg_Sdiff_SS_256_V100_cfg=DiFaReli_FsBg_Sdiff_SS_256_V100_inference.yaml_inv_with_sd_ds256_pt1_dstC/ema_085000/train/render_face/reverse_sampling/src={img_name}.jpg/dst=*"):
+                src_c = c_sorted[c_sorted['image_name'] == f'{img_name}.jpg'].values[0][1]
+                src_c = (src_c - min_c) / (max_c - min_c)
+                dst_c = c_sorted[c_sorted['image_name'] == dst_p.split('/')[-1].split('=')[-1]].values[0][1]
+                dst_c = (dst_c - min_c) / (max_c - min_c)
+                out += f"<p style=\"font-size:30px; \"> Src: {img_name}.jpg ({src_c}) Dst: {dst_p.split('/')[-1].split('=')[-1]} (({dst_c})) </p>"
+                tp = f"{dst_p}/Lerp_1000/n_frames=2/res_frame0.png"
+                out += f"<img src=/files/{tp} width=\"256\" height=\"256\">"
+                tp = f"{dst_p}/Lerp_1000/n_frames=2/shadm_shad_frame0.png"
+                out += f"<img src=/files/{tp} width=\"256\" height=\"256\">"
+                tp = f"{dst_p}/Lerp_1000/n_frames=2/res_frame1.png"
+                out += f"<img src=/files/{tp} width=\"256\" height=\"256\">"
+                tp = f"{dst_p}/Lerp_1000/n_frames=2/shadm_shad_frame1.png"
+                out += f"<img src=/files/{tp} width=\"256\" height=\"256\">"
+                tp = f"/data/mint/DPM_Dataset/ffhq_256_with_anno/ffhq_256/{set_}/{dst_p.split('/')[-1].split('=')[-1]}"
+                out += f"<img src=/files/{tp} width=\"256\" height=\"256\">"
+            out += "<br>"
                 
-            
-            
             # Shadow masks from ray-tracing
             tp = f'/data/mint/DPM_Dataset/ffhq_256_with_anno/shadow_masks/{set_}/{img_name}.png'
             out += f"<img src=/files/{tp} width=\"256\" height=\"256\">"
@@ -97,6 +131,8 @@ def create_app():
             tp = f'/data/mint/DPM_Dataset/ffhq_256_with_anno/shadow_diff_with_weight_simplified/vis/{set_}/{img_name}.png'
             out += f"<img src=/files/{tp} width=\"768\" height=\"256\">"
             out += "<br>"
+
+            # Generated dataset
             
             out += "<br><br>"
                 
