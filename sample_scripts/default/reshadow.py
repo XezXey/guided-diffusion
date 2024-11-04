39fce4ba228013ce45fdf1b4fa3bfb2cb9447f63
XezXey <puntawat-1@hotmail.com>
2024-11-04 12:09:37 +0700

    ggg

--- sample_scripts/default/reshadow.py
+++ sample_scripts/default/reshadow.py
@@ -119,6 +119,7 @@ def make_condition(cond, src_idx, dst_idx, n_step=2, itp_func=None):
             'img_size':cfg.img_model.image_size,
             'deca_obj':deca_obj,
             'batch_size':args.batch_size,
+            'render_batch_size':args.render_batch_size,
             'cfg':cfg,
             }  
     cond['misc'] = misc
