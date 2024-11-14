d8e8eeefb5309b1cce8775daf2a0ccc6716feeeb
xezxey <puntawat-1@hotmail.com>
2024-11-14 22:46:01 +0700

    fff

--- visualize_scripts/TPAMI/main_results/FFHQ_CastShadows/RotateSH_figures/create_rotateSH_figures.py
+++ visualize_scripts/TPAMI/main_results/FFHQ_CastShadows/RotateSH_figures/create_rotateSH_figures.py
@@ -121,6 +121,7 @@ def create_app():
         print(candidates)
         
         count = 0
+        sj_count = 0
         to_show = list(sample_pairs.items())[int(s):int(e)]
         # for k, v in sample_pairs.items():
         for ts in to_show:
@@ -135,10 +136,10 @@ def create_app():
             
             if args.res == 128:
                 shadow_area_pth = '/data/mint/DPM_Dataset/ffhq_256_with_anno/shadow_diff_SS_with_c_simplified/vis/'
-                out += f"[#{k}] {src}=>{dst} : <img src=/files/{data_path}/{src.replace('jpg', 'png')}>, {dst} : <img src=/files/{data_path}/{dst.replace('jpg', 'png')}>" + ", Shadow area = " + f"<img height=\"128\" src=/files/{shadow_area_pth}/{args.set_}/{src.replace('jpg', 'png')}>" + "<br>" + "<br>"
+                out += f"{int(s) + count}.[#{k}] {src}=>{dst} : <img src=/files/{data_path}/{src.replace('jpg', 'png')}>, {dst} : <img src=/files/{data_path}/{dst.replace('jpg', 'png')}>" + ", Shadow area = " + f"<img height=\"128\" src=/files/{shadow_area_pth}/{args.set_}/{src.replace('jpg', 'png')}>" + "<br>" + "<br>"
             else:
                 shadow_area_pth = '/data/mint/DPM_Dataset/ffhq_256_with_anno/shadow_diff_SS_with_c_simplified/vis/'
-                out += f"[#{k}] {src}=>{dst} : <img src=/files/{data_path}/{src}>, {dst} : <img src=/files/{data_path}/{dst}>" + ", Shadow area = " + f"<img height=\"256\" src=/files/{shadow_area_pth}/{args.set_}/{src.replace('jpg', 'png')}>" + "<br>" + "<br>"
+                out += f"{int(s) + count}.[#{k}] {src}=>{dst} : <img src=/files/{data_path}/{src}>, {dst} : <img src=/files/{data_path}/{dst}>" + ", Shadow area = " + f"<img height=\"256\" src=/files/{shadow_area_pth}/{args.set_}/{src.replace('jpg', 'png')}>" + "<br>" + "<br>"
             # Model 
             for m_idx, metadat in candidates.items():
                 # Model's metadata
