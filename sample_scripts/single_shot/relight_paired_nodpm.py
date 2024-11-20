65870f25522244dedfd6c4f8aa8ebf2f0a18618b
XezXey <puntawat-1@hotmail.com>
2024-11-21 00:32:40 +0700

    ggg

--- sample_scripts/single_shot/relight_paired_nodpm.py
+++ sample_scripts/single_shot/relight_paired_nodpm.py
@@ -316,7 +316,7 @@ if __name__ == '__main__':
         img_ext = '.png'
         cfg.dataset.training_data = 'HoloRelighting'
         cfg.dataset.data_dir = f'{cfg.dataset.root_path}/{cfg.dataset.training_data}/images_aligned/'
-        cfg.dataset.face_segment_dir = f"{cfg.dataset.root_path}/{cfg.dataset.training_data}/face_segment/"
+        cfg.dataset.face_segment_dir = f"{cfg.dataset.root_path}/{cfg.dataset.training_data}/face_segment_with_pupil/"
     elif args.dataset == 'ffhq':
         cfg.dataset.root_path = f'/data/mint/DPM_Dataset/'
         img_dataset_path = f"/data/mint/DPM_Dataset/ffhq_256_with_anno/ffhq_256/"
