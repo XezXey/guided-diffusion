fa8feff42cc729651652beb57eb8a2fe49d8c5d4
XezXey <puntawat-1@hotmail.com>
2024-11-26 11:35:12 +0700

    fff

--- visualize_scripts/TPAMI/main_results/FFHQ_CastShadows/Holo/vis_targetSH.py
+++ visualize_scripts/TPAMI/main_results/FFHQ_CastShadows/Holo/vis_targetSH.py
@@ -131,10 +131,10 @@ def create_app():
             
             if args.res == 128:
                 shadow_area_pth = '/data/mint/DPM_Dataset/HoloRelighting/shadow_diff_SS_with_c_simplified/vis/'
-                out += f"[#{k}] {src}=>{dst} : <img src=/files/{data_path}/{src.replace('jpg', 'png')}>, {dst} : <img src=/files/{data_path}/{dst.replace('jpg', 'png')}>, {gt} : <img src=/files/{data_path_noaligned}/{dst.replace('jpg', 'png')}>" + ", Shadow area = " + f"<img height=\"128\" src=/files/{shadow_area_pth}/{args.set_}/{src.replace('jpg', 'png')}>" + "<br>" + "<br>"
+                out += f"[#{k}] {src}=>{dst} : <img src=/files/{data_path}/{src.replace('jpg', 'png')}>, {dst} : <img src=/files/{data_path_noaligned}/{dst.replace('jpg', 'png')}>, {gt} : <img src=/files/{data_path_noaligned}/{dst.replace('jpg', 'png')}>" + ", Shadow area = " + f"<img height=\"128\" src=/files/{shadow_area_pth}/{args.set_}/{src.replace('jpg', 'png')}>" + "<br>" + "<br>"
             else:
                 shadow_area_pth = '/data/mint/DPM_Dataset/HoloRelighting/shadow_diff_SS_with_c_simplified/vis/'
-                out += f"[#{k}] {src}=>{dst} : <img src=/files/{data_path}/{src}>, {dst} : <img src=/files/{data_path}/{dst}>, {gt} : <img src=/files/{data_path_noaligned}/{gt}>" + ", Shadow area = " + f"<img height=\"256\" src=/files/{shadow_area_pth}/{args.set_}/{src.replace('jpg', 'png')}>" + "<br>" + "<br>"
+                out += f"[#{k}] {src}=>{dst} : <img src=/files/{data_path}/{src}>, {dst} : <img src=/files/{data_path_noaligned}/{dst}>, {gt} : <img src=/files/{data_path_noaligned}/{gt}>" + ", Shadow area = " + f"<img height=\"256\" src=/files/{shadow_area_pth}/{args.set_}/{src.replace('jpg', 'png')}>" + "<br>" + "<br>"
             # Model 
             for m_idx, metadat in candidates.items():
                 # Model's metadata
