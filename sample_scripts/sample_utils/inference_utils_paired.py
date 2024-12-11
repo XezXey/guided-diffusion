7dadc2a27df554da16af5b06956293ec96d27b4c
XezXey <puntawat-1@hotmail.com>
2024-12-11 14:14:58 +0700

    ggg

--- sample_scripts/sample_utils/inference_utils_paired.py
+++ sample_scripts/sample_utils/inference_utils_paired.py
@@ -455,9 +455,9 @@ def build_condition_image(cond, misc, force_render=False):
                 flame_face_scalp = params_utils.load_flame_mask(['face', 'scalp', 'left_eyeball', 'right_eyeball'])
                 deca_obj_face_scalp = params_utils.init_deca(mask=flame_face_scalp, rasterize_type=args.rasterize_type) # Init DECA with mask only once
                 load_deca_for_shadow_time = time.time() - load_deca_for_shadow_time
-            if args.rotate_sh_axis == 0 and args.rotate_sh:
+            if args.rotate_sh_axis == 0 and (args.rotate_sh or args.rotate_sh_dst):
                 print("[#] Fixing the axis 0 by negate ray[0]...")
-            elif args.rotate_sh_axis == 1 and args.rotate_sh:
+            elif args.rotate_sh_axis == 1 and (args.rotate_sh or args.rotate_sh_dst):
                 print("[#] Fixing the axis 1 by negate ray[1]...")
             
             start_sub_render_shadow_t = time.time()
