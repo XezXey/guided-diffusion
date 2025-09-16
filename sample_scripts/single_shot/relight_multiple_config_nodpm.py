3c21504f835c6e84ef9801a301f416c9cc36eac2
XezXey <puntawat-1@hotmail.com>
2025-09-17 01:12:04 +0700

    aa

--- sample_scripts/single_shot/relight_multiple_config_nodpm.py
+++ sample_scripts/single_shot/relight_multiple_config_nodpm.py
@@ -32,6 +32,7 @@ parser.add_argument('--relight_with_dst_c', action='store_true', default=False)
 parser.add_argument('--relight_with_src_c', action='store_true', default=False)
 parser.add_argument('--relight_with_given_c', action='store_true', default=False)
 parser.add_argument('--c', nargs='+', type=float, default=[0.7])
+parser.add_argument('--scale_sh', nargs='+', type=float, default=[1.0])
 parser.add_argument('--adjust_contrast', nargs='+', type=float, default=None, help='adjust the contrast of rendered images, two values: down, up')
 args = parser.parse_args()
 
@@ -60,65 +61,69 @@ assert args.relight_with_dst_c + args.relight_with_src_c + args.relight_with_giv
 if args.adjust_contrast is not None:
     # Given a single list make it many-to-many e.g., [0, 1, 2, 3] -> [0, 0], [0, 1], [0, 2], [0, 3], [1, 0], ..., [3, 3]
     adjust_contrast = [[x, y] for x in args.adjust_contrast for y in args.adjust_contrast]
+else:
+    adjust_contrast = [None]
 
 for ckpt in args.ckpt_step:
     for dataset in args.dataset:
         for sample_pair_json in args.sample_pair_json:
             for sdiff_dir in args.sdiff_dir:
                 for scale_depth in args.scale_depth:
-                    for cval in args.c:
-                        for adj_contrast in adjust_contrast:
-                            logger.warning("#"*100)
-                            logger.info(f'[#] Running checkpoint {ckpt}...')
-                            logger.info(f'[#] Dataset: {dataset}')
-                            logger.info(f'[#] Sample pair json: {sample_pair_json}')
-                            logger.info(f'[#] Shadow difference directory: {sdiff_dir}')
-                            logger.info(f'[#] Scale depth: {scale_depth}')
-                            logger.info(f'[#] c value: {cval}')
-                            logger.info(f'[#] Rotate SH: {args.rotate_sh}, axis: {args.rotate_sh_axis}')
-                            logger.info(f'[#] Adjust contrast of rendered images: {adj_contrast is not None}')
-                            
-                            cmd = (
-                                f"""
-                                python relight_paired_nodpm.py --ckpt_selector {args.ckpt_type} --dataset {dataset} --set valid --step {ckpt} --out_dir {args.out_dir} \
-                                --cfg_name {args.cfg_name} --log_dir {args.model_dir} \
-                                --seed 47 --render_batch_size {args.itp_step} --save_vid\
-                                --sample_pair_json {sample_pair_json} --sample_pair_mode pair \
-                                --itp {args.itp} --itp_step {args.itp_step} --batch_size {args.batch_size} --gpu_id {args.gpu_id} --lerp --idx {args.sample_idx[0]} {args.sample_idx[1]} \
-                                --postproc_shadow_mask_smooth --inverse_with_shadow_diff --shadow_diff_dir {sdiff_dir}  \
-                                --pt_round 1 --scale_depth {scale_depth} --rasterize_type {args.rasterize_type}\
-                                """
-                                )
-                            if args.force_render: cmd += ' --force_render'
-                            if args.eval_dir is not None: cmd += f' --eval_dir {args.eval_dir}'
-                            
-                            if args.relight_with_given_c:
-                                logger.info(f'[#] Relighting with given c: {cval}')
-                                cmd += f' --relight_with_given_c {cval}'
-                                pf = f'{cval}C'
-                            elif args.relight_with_dst_c:
-                                logger.info(f'[#] Relighting with dst c.')
-                                cmd += f' --relight_with_dst_c'
-                                pf = f'dstC'
-                            elif args.relight_with_src_c:
-                                logger.info(f'[#] Relighting with src c.')
-                                pf = f'srcC'
-                                # No additional argument needed, default is src c
-                            else: raise ValueError('[#] One of --relight_with_dst_c, --relight_with_src_c, --relight_with_given_c must be set.')
-                            
-                            if args.rotate_sh:
-                                logger.info(f'[#] Rotating SH, axis: {args.rotate_sh_axis}')
-                                cmd += f' --rotate_sh --rotate_sh_axis {args.rotate_sh_axis}'
-                                pf = f'rot{args.rotate_sh_axis}_{pf}'
+                    for scale_sh in args.scale_sh:
+                        for cval in args.c:
+                            for adj_contrast in adjust_contrast:
+                                logger.warning("#"*100)
+                                logger.info(f'[#] Running checkpoint {ckpt}...')
+                                logger.info(f'[#] Dataset: {dataset}')
+                                logger.info(f'[#] Sample pair json: {sample_pair_json}')
+                                logger.info(f'[#] Shadow difference directory: {sdiff_dir}')
+                                logger.info(f'[#] Scale depth: {scale_depth}')
+                                logger.info(f'[#] c value: {cval}')
+                                logger.info(f'[#] Rotate SH: {args.rotate_sh}, axis: {args.rotate_sh_axis}')
+                                logger.info(f'[#] Scale SH: {scale_sh}')
+                                logger.info(f'[#] Adjust contrast of rendered images: {adj_contrast is not None}')
                                 
-                            if adj_contrast is not None:
-                                logger.info(f'[#] Adjusting the contrast of rendered images with: down={adj_contrast[0]}, up={adj_contrast[1]}.')
-                                cmd += f' --adjust_contrast {adj_contrast[0]} {adj_contrast[1]}'
-                                pf = f'con{adj_contrast[0]}-{adj_contrast[1]}_{pf}'
-                            
-                            if postfix != '': cmd += f' --postfix SD{scale_depth}_{postfix}_{pf}'
-                            else: cmd += f' --postfix SD{scale_depth}_{pf}'
-                            
-                            logger.info(f"cmd: {cmd}")
-                            os.system(cmd)
-                            logger.warning("#"*100)
+                                cmd = (
+                                    f"""
+                                    python relight_paired_nodpm.py --ckpt_selector {args.ckpt_type} --dataset {dataset} --set valid --step {ckpt} --out_dir {args.out_dir} \
+                                    --cfg_name {args.cfg_name} --log_dir {args.model_dir} \
+                                    --seed 47 --render_batch_size {args.itp_step} --save_vid\
+                                    --sample_pair_json {sample_pair_json} --sample_pair_mode pair \
+                                    --itp {args.itp} --itp_step {args.itp_step} --batch_size {args.batch_size} --gpu_id {args.gpu_id} --lerp --idx {args.sample_idx[0]} {args.sample_idx[1]} \
+                                    --postproc_shadow_mask_smooth --inverse_with_shadow_diff --shadow_diff_dir {sdiff_dir}  \
+                                    --pt_round 1 --scale_depth {scale_depth} --rasterize_type {args.rasterize_type} --scale_sh {scale_sh}\
+                                    """
+                                    )
+                                if args.force_render: cmd += ' --force_render'
+                                if args.eval_dir is not None: cmd += f' --eval_dir {args.eval_dir}'
+                                
+                                if args.relight_with_given_c:
+                                    logger.info(f'[#] Relighting with given c: {cval}')
+                                    cmd += f' --relight_with_given_c {cval}'
+                                    pf = f'{cval}C'
+                                elif args.relight_with_dst_c:
+                                    logger.info(f'[#] Relighting with dst c.')
+                                    cmd += f' --relight_with_dst_c'
+                                    pf = f'dstC'
+                                elif args.relight_with_src_c:
+                                    logger.info(f'[#] Relighting with src c.')
+                                    pf = f'srcC'
+                                    # No additional argument needed, default is src c
+                                else: raise ValueError('[#] One of --relight_with_dst_c, --relight_with_src_c, --relight_with_given_c must be set.')
+                                
+                                if args.rotate_sh:
+                                    logger.info(f'[#] Rotating SH, axis: {args.rotate_sh_axis}')
+                                    cmd += f' --rotate_sh --rotate_sh_axis {args.rotate_sh_axis}'
+                                    pf = f'{pf}_rot{args.rotate_sh_axis}'
+                                    
+                                if adj_contrast is not None:
+                                    logger.info(f'[#] Adjusting the contrast of rendered images with: down={adj_contrast[0]}, up={adj_contrast[1]}.')
+                                    cmd += f' --adjust_contrast {adj_contrast[0]} {adj_contrast[1]}'
+                                    pf = f'con{adj_contrast[0]}-{adj_contrast[1]}_{pf}'
+                                
+                                if postfix != '': cmd += f' --postfix SD{scale_depth}_{postfix}_{pf}'
+                                else: cmd += f' --postfix {pf}_{scale_sh}sh'
+                                
+                                logger.info(f"cmd: {cmd}")
+                                os.system(cmd)
+                                logger.warning("#"*100)
